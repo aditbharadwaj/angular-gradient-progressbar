d6a92eaf199a56813dda45fca4ef4828c2c4ecfc
aditbharadwaj <tidabharadwaj04@gmail.com>
2024-04-01 20:54:43 +0530

    Updated to angular v17

--- src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-gradient-progressbar
- */
-
-
-export * from './lib/angular-gradient-progressbar.component';
-export * from './lib/angular-gradient-progressbar.module';
